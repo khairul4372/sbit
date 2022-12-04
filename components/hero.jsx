65041eba2c6911002c7e2c1767080a02ec333800
Khairul Anam <khairul.anam@pantheon.io>
2022-12-05 05:05:53 +0600

    hero section responsive

--- components/hero.jsx
+++ components/hero.jsx
@@ -5,12 +5,40 @@ import styles from "../styles/hero.module.css";
 const Hero = () => {
   return (
     <>
+      {/* <div className="row">
+        <Link className="shadow col p-4" href="/outsourcing">
+          OUTSOURCING MODULE
+        </Link>
+        <Link className="shadow col p-2" href="/cadence">
+          CADENCE UNIVERSITY PROGRAM IN BANGLADESH
+        </Link>
+        <Link className="shadow col p-2" href="/mobile">
+          MOBILE APP DEVELOPMENT
+        </Link>
+        <Link className="shadow col p-2" href="/news">
+          NEWS
+        </Link>
+        <Link className="shadow col p-2" href="/training">
+          TRAINING PROGRAM
+        </Link>
+      </div>
+ */}
       <div className={styles.hero}>
-        <Link href="/outsourcing">OUTSOURCING MODULE</Link>
-        <Link href="/cadence">CADENCE UNIVERSITY PROGRAM IN BANGLADESH</Link>
-        <Link href="/mobile">MOBILE APP DEVELOPMENT</Link>
-        <Link href="/news">NEWS</Link>
-        <Link href="/training">TRAINING PROGRAM</Link>
+        <Link className={styles.hero_link} href="/outsourcing">
+          <p className={styles.link_text}>OUTSOURCING MODULE</p>
+        </Link>
+        <Link className={styles.hero_link} href="/cadence">
+          <p className={styles.link_text}>CADENCE UNIVERSITY PROGRAM</p>
+        </Link>
+        <Link className={styles.hero_link} href="/mobile">
+          <p className={styles.link_text}>MOBILE APP DEVELOPMENT</p>
+        </Link>
+        <Link className={styles.hero_link} href="/news">
+          <p className={styles.link_text}>NEWS</p>
+        </Link>
+        <Link className={styles.hero_link} href="/training">
+          <p className={styles.link_text}>TRAINING PROGRAM</p>
+        </Link>
       </div>
     </>
   );
