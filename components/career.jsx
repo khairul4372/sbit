12f5b40389674151d415d0aa1d6222901ddb7b44
Khairul Anam <khairul.anam@pantheon.io>
2023-03-20 21:52:57 -0700

    complete career page and fix route issue

--- components/career.jsx
+++ components/career.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import Link from "next/link";
-import styles from "../styles/contact.module.css";
+import styles from "../styles/career.module.css";
 const Career = () => {
   return (
-    <div className={styles.page_body}>
-      <div className={styles.page_main}>
-        <div className={styles.page_address}>
+    <div>
+      <div>
+        <div className={styles.c_body}>
           <h4>Job Openings </h4>
           <p>Computer Systems Engineer/Architects</p>
 
-          <Link
-            className="text-decoration-none text-reset"
-            href="/assets/pdfs/Job_Posting_by_the_Petitioner_for_this-position.pdf"
-          >
+          <Link href="/assets/pdfs/Job_Posting_by_the_Petitioner_for_this-position.pdf">
             Check here
           </Link>
         </div>
